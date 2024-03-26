ca211e6e37f0129b9735d0e229a2c937df3815f2
Fixer <67309088+leelang7@users.noreply.github.com>
2024-03-26 13:25:13 +0900

    Update chatbot_240325.py

--- chatbot_240325.py
+++ chatbot_240325.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from streamlit_chat import message  # 설치(파이썬 3.8 이상에서만 작동)
+from streamlit_chat import message # 설치(파이썬 3.8 이상에서만 작동)
 import pandas as pd
-from sentence_transformers import SentenceTransformer  # sentence_transformer 설치 필요
+from sentence_transformers import SentenceTransformer #sentence_transformer 설치 필요
 from sklearn.metrics.pairwise import cosine_similarity
 import json
 from movie import recommend_movie
@@ -24,12 +24,12 @@ st.header('심리상담 챗봇')
 st.markdown("[코리아 IT 아카데미], 이석창 강사)")
 
 if 'generated' not in st.session_state:
-    st.session_state['generated'] = []  # 대화한 내용 저장
+    st.session_state['generated'] = [] # 대화한 내용 저장
 
 if 'past' not in st.session_state:
-    st.session_state['past'] = []  # 지난 대화 저장
+    st.session_state['past'] = [] # 지난 대화 저장
 
-with st.form('form', clear_on_submit=True):  # 사용자 입력 폼
+with st.form('form', clear_on_submit=True): # 사용자 입력 폼
     user_input = st.text_input('당신: ', '')
     submitted = st.form_submit_button('전송')
 
@@ -55,9 +55,8 @@ if submitted and user_input:
 for i in range(len(st.session_state['past'])):
     message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
     if len(st.session_state['generated']) > i:
-        st.text(st.session_state['generated'][i])
-        #message(st.session_state['generated'][i], key=str(i) + '_bot')
-
-# 사용자가 전송 버튼을 클릭한 경우에만 UI를 갱신
-if submitted:
-    st.experimental_rerun()
+        if isinstance(st.session_state['generated'][i], list):  # 만약 영화 추천 결과인 경우
+            bot_response = "<br>".join([f"<b>{title}</b> ({score}) - {', '.join(genres)}" for title, score, genres in st.session_state['generated'][i]])
+            st.markdown(f"<div style='background-color: #333333; color: white; padding: 10px; border-radius: 5px;'><b>Bot:</b><br>{bot_response}</div>", unsafe_allow_html=True)
+        else:  # 일반적인 챗봇 응답인 경우
+            st.markdown(f"<div style='background-color: #333333; color: white; padding: 10px; border-radius: 5px;'><b>Bot:</b> {st.session_state['generated'][i]}</div>", unsafe_allow_html=True)
